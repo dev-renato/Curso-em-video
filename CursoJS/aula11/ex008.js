8b81aeb89a06bc70f7ea596fad85de1d71c24a4d
Renato Silva <renatogithub@gmail.com>
2020-10-13 20:48:11 -0300

    ex008
    Exercico 008 Condicional Simples

---
+++ CursoJS/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if(vel > 60) {
+    console.log('Voce ultrapassou a volocidade permitida. Multado!')
+}
+console.log('Diriga sempre com cinto de segurança!');
