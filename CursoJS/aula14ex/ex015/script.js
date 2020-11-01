8f2c9aea191de8b1d5d745c93db5c34e344d2441
Renato Silva <49454767+dev-renato@users.noreply.github.com>
2020-11-01 18:22:33 -0300

    ex015
    Finalizando exercicio 015

--- CursoJS/aula14ex/ex015/script.js
+++ CursoJS/aula14ex/ex015/script.js
@@ -9,6 +9,34 @@ function verificador() {
     } else {
         let fsex = document.getElementsByName('radsex')
         let idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
-    }
+        let genero = ''
+        let img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //colocar a imagem. Mas eu nao queria ter que editar.
+            } else if (idade < 21) {
+                //colocar a imagem. Mas eu nao queria ter que editar.
+            } else if (idade < 50) {
+                //colocar a imagem. Mas eu nao queria ter que editar.
+            } else {
+                //colocar a imagem. Mas eu nao queria ter que editar.
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //colocar a imagem. Mas eu nao queria ter que editar.
+            } else if (idade < 21) {
+                //colocar a imagem. Mas eu nao queria ter que editar.
+            } else if (idade < 50) {
+                //colocar a imagem. Mas eu nao queria ter que editar.
+            } else {
+                //colocar a imagem. Mas eu nao queria ter que editar.    
+            }
+        }   
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }    
 }
